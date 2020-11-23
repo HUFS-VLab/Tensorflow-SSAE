586416592c98e557f608fb3ee1d0ea92792ea41f
Nam Ki Hyun <devkihyun@naver.com>
2020-11-23 22:26:24 +0900

    Create run_ssae_trainer.sh
    Update the shell script file that run to train model.

---
+++ script/run_ssae_trainer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+MAIN_DIR=${0%/*}
+cd $MAIN_DIR/..
+MAIN_DIR=${0%/*}
+
+TARGET_CODE=run.py
+DATASET_NAME=SMD_2020
+MANIFEST_PATH=manifests/${DATASET_NAME}
+
+TRAIN_MANIFESTS="${MANIFEST_PATH}/GT-4118.json" 
+#TRAIN_MANIFESTS="${MANIFEST_PATH}/ST-3214.json" 
+#TRAIN_MANIFESTS="${MANIFEST_PATH}/ST-3708.json" 
+    
+MODEL_PATH=model
+
+# HYPER PARAMETERS
+SEQ_LEN=32
+N_MELS=80
+#THRES_WEIGHT=2.0
+
+#RNN
+N_LAYERS=4
+EPOCHS=5000
+
+DATASET_PATH=seqlen_${SEQ_LEN}_mels_${N_MELS}
+
+python -u $TARGET_CODE \
+--train-manifests $TRAIN_MANIFESTS \
+--dataset-name $DATASET_NAME \
+--dataset-path $DATASET_PATH \
+--seq_len $SEQ_LEN \
+--n-mels $N_MELS \
+--n-layers $N_LAYERS \
+--epochs $EPOCHS \
+--model-path $MODEL_PATH
